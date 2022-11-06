801848504dc70f430efcd09b821e142697e06e1d
Vitholu <116558222+Vitholu@users.noreply.github.com>
2022-11-06 19:21:21 +0100

    Add tab switching logic

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 import { initialLoad } from './onLoad.js';
+import { initialLoad2 } from './onLoad2.js';
+import { initialLoad3 } from './onLoad3.js';
 console.log('Webpack Works');
 
-initialLoad();
+const versionToggle = document.querySelectorAll('button');
+
+versionToggle.forEach((e) => {
+	e.addEventListener('click', (e) => {
+		if (e.target.id === '2') {
+			initialLoad2(true);
+			console.log('okaaay');
+		}
+		if (e.target.id === '3') {
+			initialLoad3(true);
+			console.log('okaaay');
+		}
+	});
+});
